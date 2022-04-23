8e8f827d2f512b7e9b8c66fc2ed461aabe3f67bd
Asuki Kono <asukiaaa@gmail.com>
2022-04-23 10:56:20 +0900

    Initial commit after creating by clj-new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-pedestal-reload-practice
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/asuki/clj_pedestal_reload_practice_test.clj
@@ -0,0 +1,7 @@
+(ns asuki.clj-pedestal-reload-practice-test
+  (:require [clojure.test :refer :all]
+            [asuki.clj-pedestal-reload-practice :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
