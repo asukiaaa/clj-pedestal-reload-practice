31e4c64450ca6cee69405208164a8dfa247683ce
Asuki Kono <asukiaaa@gmail.com>
2022-04-24 12:16:26 +0900

    Created a sample pedestal server

--- src/asuki/clj_pedestal_reload_practice.clj
+++ src/asuki/clj_pedestal_reload_practice.clj
@@ -1,12 +1,25 @@
 (ns asuki.clj-pedestal-reload-practice
-  (:gen-class))
+  (:gen-class)
+  (:require [io.pedestal.http :as http]))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(defn handler-top [req]
+  {:status 200
+   :body "hello world"})
+
+(def main-route
+  #{["/" :get handler-top :route-name :top]})
+
+(defn start-server []
+  (let [host "0.0.0.0"
+        port 3000]
+    (println (format "starting server as http://%s:%d" host port))
+    (-> {::http/routes main-route
+         ::http/port 3000
+         ::http/host host
+         ::http/type :jetty}
+        http/create-server
+        http/start)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (greet {:name (first args)}))
+  (start-server))
