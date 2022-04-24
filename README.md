fa9a2cf22c9aa84b4496eb665f8d692130770a62
Asuki Kono <asukiaaa@gmail.com>
2022-04-24 13:55:31 +0900

    Apply autoreload for development

--- README.md
+++ README.md
@@ -13,11 +13,17 @@ Run main.
 clj -M:run-m
 ```
 
+Run development mode to watch src dir.
+```
+clj -X:dev
+```
+
 ## License
 
 Copyright © 2022 Asuki
 Distributed under the Eclipse Public License version 1.0.
 
 ## References
-
+- [Watch and auto reload a Clojure pedestal service on source change](https://dev.to/praburajan/watch-and-auto-reload-a-clojure-pedestal-service-on-save-4ehl)
+- [pedestal/samples/auto-reload-server/dev/dev.clj](https://github.com/pedestal/samples/blob/master/auto-reload-server/dev/dev.clj)
 - [Clojureサービス開発ライブラリPedestal入門](https://qiita.com/lagenorhynque/items/fbd66ebaa0352ec4253d)
--- src/asuki/clj_pedestal_reload_practice.clj
+++ src/asuki/clj_pedestal_reload_practice.clj
@@ -1,6 +1,8 @@
 (ns asuki.clj-pedestal-reload-practice
   (:gen-class)
-  (:require [io.pedestal.http :as http]))
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [ns-tracker.core :refer [ns-tracker]]))
 
 (defn handler-top [req]
   {:status 200
@@ -9,16 +11,33 @@
 (def main-route
   #{["/" :get handler-top :route-name :top]})
 
-(defn start-server []
+(def watched-namespaces
+  (ns-tracker "src"))
+
+;; define target-route out of function to relodable
+(def target-route main-route)
+
+(defn watched-target-route []
+  (doseq [ns-sym (watched-namespaces)]
+    #_(println "reload " ns-sym)
+    (require ns-sym :reload))
+  (route/expand-routes target-route))
+
+(defn start-server [& args]
   (let [host "0.0.0.0"
-        port 3000]
+        port 3000
+        watch (when (some? args) (.contains args :watch))]
     (println (format "starting server as http://%s:%d" host port))
-    (-> {::http/routes main-route
+    (when watch (println "with watching"))
+    (-> {::http/routes (if watch watched-target-route target-route)
          ::http/port 3000
          ::http/host host
          ::http/type :jetty}
         http/create-server
         http/start)))
+
+(defn run-dev [& args]
+  (start-server :watch))
 
 (defn -main
   [& args]
